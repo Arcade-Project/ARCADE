1472fe38e3c349c20591e978a931bbe8bb257c92
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2022-12-27 23:24:26 +0100

    update mobile version

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 const menuHamburger = document.querySelector(".menu-hamburger");
 const navLinks = document.querySelector(".nav-links");
+const cross = document.querySelector(".cross");
 
 menuHamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("nav-links-active");
+  navLinks.classList.add("nav-links-active");
+  menuHamburger.classList.add("menuHamburger-active");
+  cross.classList.add("cross-active");
 });
-
+cross.addEventListener("click", () => {
+  navLinks.classList.remove("nav-links-active");
+  menuHamburger.classList.remove("menuHamburger-active");
+  cross.classList.remove("cross-active");
+});
 // Contact Part
 
 const contact = document.getElementById("contact");
