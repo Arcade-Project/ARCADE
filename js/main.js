21a1ef6915d4e5be51910453f8aed4e31d54f29b
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2023-01-03 15:27:39 +0100

    update about page

---
+++ js/main.js
@@ -0,0 +1,36 @@
+window.onload = (event) => {
+  console.log("page is fully loaded");
+  const menuHamburger = document.querySelector(".menu-hamburger");
+  const navbar = document.querySelector(".navbar");
+  const cross = document.querySelector(".cross");
+
+  menuHamburger.addEventListener("click", () => {
+    navbar.classList.add("navbar-active");
+    menuHamburger.classList.add("menuHamburger-active");
+    cross.classList.add("cross-active");
+  });
+
+  cross.addEventListener("click", () => {
+    navbar.classList.remove("navbar-active");
+    menuHamburger.classList.remove("menuHamburger-active");
+    cross.classList.remove("cross-active");
+  });
+
+  // Index Part
+
+  // let wiki = document.querySelector(".wiki");
+  // let usefulLinks = document.querySelector(".useful-links");
+  // let githubArcade = document.querySelector(".github-arcade");
+
+  // wiki.addEventListener(
+  //   "mouseover",
+  //   (e) => {
+  //     wiki.style.width = "50px";
+  //     usefulLinks.style.width = "47px";
+  //     githubArcade.style.width = "44px";
+
+  //     console.log(e.target.isTrusted);
+  //   },
+  //   false
+  // );
+};
