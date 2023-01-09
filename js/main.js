10fc25f06fa43fcfe2a3178bc71462952d5a3d28
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2023-01-09 22:51:18 +0100

    update

--- js/main.js
+++ js/main.js
@@ -15,22 +15,5 @@ window.onload = (event) => {
     menuHamburger.classList.remove("menuHamburger-active");
     cross.classList.remove("cross-active");
   });
-
-  // Index Part
-
-  // let wiki = document.querySelector(".wiki");
-  // let usefulLinks = document.querySelector(".useful-links");
-  // let githubArcade = document.querySelector(".github-arcade");
-
-  // wiki.addEventListener(
-  //   "mouseover",
-  //   (e) => {
-  //     wiki.style.width = "50px";
-  //     usefulLinks.style.width = "47px";
-  //     githubArcade.style.width = "44px";
-
-  //     console.log(e.target.isTrusted);
-  //   },
-  //   false
-  // );
+  
 };
