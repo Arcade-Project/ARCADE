762200282ac078328aa9065e9910831f181d31f6
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2023-01-18 15:35:37 +0100

    update anim

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 window.onload = (event) => {
   console.log("page is fully loaded");
   const menuHamburger = document.querySelector(".menu-hamburger");
-  const navbar = document.querySelector(".navbar");
+  const navlinks = document.querySelector(".navlinks");
   const cross = document.querySelector(".cross");
 
   menuHamburger.addEventListener("click", () => {
-    navbar.classList.add("navbar-active");
+    navlinks.classList.add("navlinks-active");
     menuHamburger.classList.add("menuHamburger-active");
     cross.classList.add("cross-active");
   });
 
   cross.addEventListener("click", () => {
-    navbar.classList.remove("navbar-active");
+    navlinks.classList.remove("navlinks-active");
     menuHamburger.classList.remove("menuHamburger-active");
     cross.classList.remove("cross-active");
   });
