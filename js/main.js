dfee768f636274bc60b20382f705ec004b6ee528
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2023-03-01 15:45:22 +0100

    update

--- js/main.js
+++ js/main.js
@@ -6,8 +6,11 @@ window.onload = (event) => {
   const cross = document.querySelector(".cross");
   const nav = document.querySelector("nav")
   const arc = document.querySelector(".logo")
+  const backexit = document.querySelector(".backexit")
 
   menuHamburger.addEventListener("click", () => {
+    nav.style.height = '100%'
+    backexit.style.display = 'block'
     arc.style.position = 'fixed'
     navlinks.style.display = 'block'
     menuHamburger.classList.add("menuHamburger-active");
@@ -15,10 +18,21 @@ window.onload = (event) => {
   });
 
   cross.addEventListener("click", () => {
+    nav.style.height = '80px'
+    backexit.style.display = 'none'
     arc.style.position = 'absolute'
     navlinks.style.display = 'none'
     menuHamburger.classList.remove("menuHamburger-active");
     cross.classList.remove("cross-active");
+  });
+
+    backexit.addEventListener("click", () => {
+      nav.style.height = '80px'
+      backexit.style.display = 'none'
+      arc.style.position = 'absolute'
+      navlinks.style.display = 'none'
+      menuHamburger.classList.remove("menuHamburger-active");
+      cross.classList.remove("cross-active");
   });
   
 };
