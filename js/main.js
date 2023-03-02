da453400fbfedde661b0a4179ba692f30c368943
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2023-03-02 20:07:45 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,38 +1,35 @@
-window.onload = (event) => {
-  console.log("page is fully loaded");
 
-  const menuHamburger = document.querySelector(".menu-hamburger");
-  const navlinks = document.querySelector(".navlinks");
-  const cross = document.querySelector(".cross");
-  const nav = document.querySelector("nav")
-  const arc = document.querySelector(".logo")
-  const backexit = document.querySelector(".backexit")
+const menuHamburger = document.querySelector(".menu-hamburger");
+const navlinks = document.querySelector(".navlinks");
+const cross = document.querySelector(".cross");
+const nav = document.querySelector("nav")
+const arc = document.querySelector(".logo")
+const backexit = document.querySelector(".backexit")
 
-  menuHamburger.addEventListener("click", () => {
-    nav.style.height = '100%'
-    backexit.style.display = 'block'
-    arc.style.position = 'fixed'
-    navlinks.style.display = 'block'
-    menuHamburger.classList.add("menuHamburger-active");
-    cross.classList.add("cross-active");
-  });
+menuHamburger.addEventListener("click", () => {
+  nav.style.height = '100%'
+  backexit.style.display = 'block'
+  arc.style.position = 'fixed'
+  navlinks.style.display = 'block'
+  menuHamburger.classList.add("menuHamburger-active");
+  cross.classList.add("cross-active");
+});
 
-  cross.addEventListener("click", () => {
+cross.addEventListener("click", () => {
+  nav.style.height = '80px'
+  backexit.style.display = 'none'
+  arc.style.position = 'absolute'
+  navlinks.style.display = 'none'
+  menuHamburger.classList.remove("menuHamburger-active");
+  cross.classList.remove("cross-active");
+});
+
+  backexit.addEventListener("click", () => {
     nav.style.height = '80px'
     backexit.style.display = 'none'
     arc.style.position = 'absolute'
     navlinks.style.display = 'none'
     menuHamburger.classList.remove("menuHamburger-active");
     cross.classList.remove("cross-active");
-  });
+});
 
-    backexit.addEventListener("click", () => {
-      nav.style.height = '80px'
-      backexit.style.display = 'none'
-      arc.style.position = 'absolute'
-      navlinks.style.display = 'none'
-      menuHamburger.classList.remove("menuHamburger-active");
-      cross.classList.remove("cross-active");
-  });
-  
-};
