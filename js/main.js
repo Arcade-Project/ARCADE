67edd12c8265153dd31198c12136ecaaf1285a3d
Itsoon <91259186+Itsoon-xyz@users.noreply.github.com>
2023-03-02 21:08:18 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,35 +1,45 @@
+window.onload = (event) => {
+  console.log("page is fully loaded");
 
-const menuHamburger = document.querySelector(".menu-hamburger");
-const navlinks = document.querySelector(".navlinks");
-const cross = document.querySelector(".cross");
-const nav = document.querySelector("nav")
-const arc = document.querySelector(".logo")
-const backexit = document.querySelector(".backexit")
+  const menuHamburger = document.querySelector(".menu-hamburger");
+  const navlinks = document.querySelector(".navlinks");
+  const cross = document.querySelector(".cross");
+  const nav = document.querySelector("nav")
+  const logo = document.querySelector(".logo")
+  const backexit = document.querySelector(".backexit")
+  const btndownload = document.querySelector(".btnr")
 
-menuHamburger.addEventListener("click", () => {
-  nav.style.height = '100%'
-  backexit.style.display = 'block'
-  arc.style.position = 'fixed'
-  navlinks.style.display = 'block'
-  menuHamburger.classList.add("menuHamburger-active");
-  cross.classList.add("cross-active");
-});
+  menuHamburger.addEventListener("click", () => {
+    btndownload.style.visibility = 'visible'
+    logo.classList.add("logofix")
+    nav.style.height = '100%'
+    backexit.style.display = 'block'
+    logo.style.position = 'fixed'
+    navlinks.style.display = 'block'
+    menuHamburger.classList.add("menuHamburger-active");
+    cross.classList.add("cross-active");
+  });
 
-cross.addEventListener("click", () => {
-  nav.style.height = '80px'
-  backexit.style.display = 'none'
-  arc.style.position = 'absolute'
-  navlinks.style.display = 'none'
-  menuHamburger.classList.remove("menuHamburger-active");
-  cross.classList.remove("cross-active");
-});
-
-  backexit.addEventListener("click", () => {
+  cross.addEventListener("click", () => {
+    btndownload.style.visibility = 'hidden'
+    logo.classList.remove("logofix")
     nav.style.height = '80px'
     backexit.style.display = 'none'
-    arc.style.position = 'absolute'
+    logo.style.position = 'absolute'
     navlinks.style.display = 'none'
     menuHamburger.classList.remove("menuHamburger-active");
     cross.classList.remove("cross-active");
-});
+  });
 
+    backexit.addEventListener("click", () => {
+      btndownload.style.visibility = 'hidden'
+      logo.classList.remove("logofix")
+      nav.style.height = '80px'
+      backexit.style.display = 'none'
+      logo.style.position = 'absolute'
+      navlinks.style.display = 'none'
+      menuHamburger.classList.remove("menuHamburger-active");
+      cross.classList.remove("cross-active");
+  });
+  
+};
